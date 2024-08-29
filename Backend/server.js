287a666abb24538ef2f8552cac6cb6972e48f1d2
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-08-29 17:12:50 +0530

    stuck while connecting frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const dbCalling = require("./config/Db");
+const cors = require('cors');
 //Routes
 const router = require("./routes/AuthenticationRoutes");
 app.use(express.json());
@@ -10,6 +11,12 @@ const port = process.env.PORT || 8000;
 
 //AUTH ROUTES
 app.use("/api/auth", router);
+
+//cors apply there 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials:true,
+}))
 
 //caled the db
 dbCalling();
--- Frontend/src/validation/SingupUi.jsx
+++ Frontend/src/validation/SingupUi.jsx
@@ -1,24 +1,78 @@
+import { setLoading, setSingupData } from "@/Store/Slice/CatalogSlice";
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import toast from "react-hot-toast";
+// import { useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { sendOtp } from "@/services/Operations/MainApiOperation";
+
+// import { FaLongArrowAltRight } from "react-icons/fa";
 
 const SingupUi = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   //create an state which handl the for valid
-  const [singupForm, setSingupForm] = useState({});
-
+  const [singupForm, setSingupForm] = useState({
+    fullName: "",
+    email: " ",
+    password: "",
+    confirmPassword: "",
+  });
+  //destruct the values
+  const { fullName, email, password, confirmPassword } = singupForm;
+  const [showPassword, setPassword] = useState(false);
+  const [ConfirmPassword, setConfirmPassword] = useState(false);
   // pending here 🤷‍♂️
-  // onchagne methods
-  const singupInputField = () => {};
+  // onchagne methods where the users can type his data
+  const singupInputField = (e) => {
+    e.preventDefault();
+    //now modified its
+    setSingupForm({
+      ...singupForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //onsubmit form submission
+  const FormSubmitted = (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+      //passweord
+      if (password !== confirmPassword) {
+        toast.error("Password not matched !");
+      }
+
+      const singupData = {
+        ...singupForm,
+      };
+
+      //reset the form
+      dispatch(setSingupData(singupData));
+      //otp move
+      dispatch(sendOtp(singupForm.email, navigate));
+      // now move to the dashboard of the product page
+      //but before otp verify
+    } catch (er) {
+      console.log("error while submitting !", er);
+      toast.error("error at singupUi");
+    }
+  };
 
   return (
     <div>
       <div className=" bg-slate-900 h-screen flex justify-center items-center  overflow-x-hidden  overflow-y-hidden">
         <section className="max-w-4xl p-7   md:w-[50%] mx-auto  bg-slate-50 rounded-md shadow-md    dark:bg-gray-800">
-          <h2 className="text-lg font-semibold capitalize flex items-center justify-center  dark:text-white">
+          <h2 className="text-lg font-semibold capitalize flex items-center justify-center  text-black">
             <span className=" text-black font-semibold border-b-2 border-red-500 ">
               welcome to Ecomxy
             </span>
           </h2>
-          <form>
+
+          {/* Main logics of form sdtart from there */}
+          <form onSubmit={FormSubmitted}>
             <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
                 <label
@@ -30,6 +84,7 @@ const SingupUi = () => {
                 <input
                   id="name"
                   name="fullName"
+                  value={fullName}
                   onChange={singupInputField}
                   type="text"
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800
@@ -49,6 +104,7 @@ const SingupUi = () => {
                   id="emailAddress"
                   type="email"
                   name="email"
+                  value={email}
                   onChange={singupInputField}
                   className="block w-full px-4 py-2 mt-2 text-gray-700
                  bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600
@@ -56,22 +112,30 @@ const SingupUi = () => {
                 />
               </div>
 
-              <div>
+              <div className=" ">
                 <label
-                  className="text-gray-700 dark:text-gray-200"
+                  className="text-gray-700  dark:text-gray-200"
                   for="password"
                 >
                   Password
                 </label>
                 <input
                   id="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   name="password"
+                  value={password}
                   onChange={singupInputField}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border
                  border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400
                   focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
+
+                <span
+                  onClick={() => setPassword((pre) => !pre)}
+                  className="  flex justify-end bg-none text-black  lg:mt-[-2rem] lg:mr-[1rem] sm:mr-[1rem]  mt-2rem z-10 "
+                >
+                  {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                </span>
               </div>
 
               <div>
@@ -83,13 +147,21 @@ const SingupUi = () => {
                 </label>
                 <input
                   id="passwordConfirmation"
-                  type="confirmPassword"
+                  type={ConfirmPassword ? "text" : "password"}
                   name="confirmPassword"
+                  value={confirmPassword}
                   onChange={singupInputField}
                   className="block w-full px-4 py-2 mt-2 text-gray-700
                  bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600
                   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
+
+                <span
+                  onClick={() => setConfirmPassword((pre) => !pre)}
+                  className=" text-black   cursor-pointer flex justify-end  "
+                >
+                  {ConfirmPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                </span>
               </div>
             </div>
             <br></br>
@@ -105,7 +177,10 @@ const SingupUi = () => {
                 </button>
               </NavLink>
 
-              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              <button
+                type="submit"
+                className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+              >
                 Singup
               </button>
             </div>
